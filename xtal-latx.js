d5777d0456495796b5d9d9e4fa7f6be1613a1d09
bahrus <anderson.bruce.b@gmail.com>
2018-06-02 18:36:24 -0400

    mixin

--- xtal-latx.js
+++ xtal-latx.js
@@ -1,78 +1,80 @@
 const pass_down = 'pass-down';
 const disabled = 'disabled';
-export class XtallatX extends HTMLElement {
-    static get observedAttributes() {
-        return [disabled, pass_down];
-    }
-    get passDown() {
-        return this._passDown;
-    }
-    set passDown(val) {
-        this.setAttribute(pass_down, val);
-    }
-    get disabled() {
-        return this._disabled;
-    }
-    set disabled(val) {
-        if (val) {
-            this.setAttribute(disabled, '');
+export function XtallatX(superClass) {
+    return class extends superClass {
+        static get observedAttributes() {
+            return [disabled, pass_down];
         }
-        else {
-            this.removeAttribute(disabled);
+        get passDown() {
+            return this._passDown;
         }
-    }
-    attributeChangedCallback(name, oldVal, newVal) {
-        switch (name) {
-            case pass_down:
-                this._passDown = newVal;
-                this.parsePassDown();
-                break;
-            case disabled:
-                this._disabled = newVal !== null;
-                break;
+        set passDown(val) {
+            this.setAttribute(pass_down, val);
         }
-    }
-    de(name, detail) {
-        const newEvent = new CustomEvent(name + '-changed', {
-            detail: detail,
-            bubbles: true,
-            composed: false,
-        });
-        this.dispatchEvent(newEvent);
-        return newEvent;
-    }
-    parsePassDown() {
-        this._cssPropMap = [];
-        const splitPassDown = this._passDown.split('};');
-        splitPassDown.forEach(passDownSelectorAndProp => {
-            if (!passDownSelectorAndProp)
-                return;
-            const splitPassTo2 = passDownSelectorAndProp.split('{');
-            this._cssPropMap.push({
-                cssSelector: splitPassTo2[0],
-                propTarget: splitPassTo2[1]
+        get disabled() {
+            return this._disabled;
+        }
+        set disabled(val) {
+            if (val) {
+                this.setAttribute(disabled, '');
+            }
+            else {
+                this.removeAttribute(disabled);
+            }
+        }
+        attributeChangedCallback(name, oldVal, newVal) {
+            switch (name) {
+                case pass_down:
+                    this._passDown = newVal;
+                    this.parsePassDown();
+                    break;
+                case disabled:
+                    this._disabled = newVal !== null;
+                    break;
+            }
+        }
+        de(name, detail) {
+            const newEvent = new CustomEvent(name + '-changed', {
+                detail: detail,
+                bubbles: true,
+                composed: false,
             });
-        });
-    }
-    passDownProp(val) {
-        let nextSibling = this.nextElementSibling;
-        while (nextSibling) {
-            this._cssPropMap.forEach(map => {
-                if (nextSibling.matches(map.cssSelector)) {
-                    nextSibling[map.propTarget] = val;
+            this.dispatchEvent(newEvent);
+            return newEvent;
+        }
+        parsePassDown() {
+            this._cssPropMap = [];
+            const splitPassDown = this._passDown.split('};');
+            splitPassDown.forEach(passDownSelectorAndProp => {
+                if (!passDownSelectorAndProp)
+                    return;
+                const splitPassTo2 = passDownSelectorAndProp.split('{');
+                this._cssPropMap.push({
+                    cssSelector: splitPassTo2[0],
+                    propTarget: splitPassTo2[1]
+                });
+            });
+        }
+        passDownProp(val) {
+            let nextSibling = this.nextElementSibling;
+            while (nextSibling) {
+                this._cssPropMap.forEach(map => {
+                    if (nextSibling.matches(map.cssSelector)) {
+                        nextSibling[map.propTarget] = val;
+                    }
+                });
+                nextSibling = nextSibling.nextElementSibling;
+            }
+        }
+        _upgradeProperties(props) {
+            props.forEach(prop => {
+                if (this.hasOwnProperty(prop)) {
+                    let value = this[prop];
+                    delete this[prop];
+                    this[prop] = value;
                 }
             });
-            nextSibling = nextSibling.nextElementSibling;
         }
-    }
-    _upgradeProperties(props) {
-        props.forEach(prop => {
-            if (this.hasOwnProperty(prop)) {
-                let value = this[prop];
-                delete this[prop];
-                this[prop] = value;
-            }
-        });
-    }
+    };
 }
 //# sourceMappingURL=xtal-latx.js.map
--- xtal-latx.ts
+++ xtal-latx.ts
@@ -6,84 +6,86 @@ export interface ICssPropMap {
     //propMapper: {[key: string]: string[]}
     propTarget: string;
 }
-export class XtallatX extends HTMLElement {
-    static get observedAttributes() {
-        return [disabled, pass_down];
-    }
-
-    _passDown: string;
-    get passDown() {
-        return this._passDown;
-    }
-    set passDown(val) {
-        this.setAttribute(pass_down, val);
-    }
-
-    _disabled: boolean;
-    get disabled() {
-        return this._disabled;
-    }
-    set disabled(val) {
-        if (val) {
-            this.setAttribute(disabled, '');
-        } else {
-            this.removeAttribute(disabled);
+export function XtallatX(superClass) {
+    return class extends superClass {
+        static get observedAttributes() {
+            return [disabled, pass_down];
         }
-    }
-
-    attributeChangedCallback(name: string, oldVal: string, newVal: string) {
-        switch (name) {
-            case pass_down:
-                this._passDown = newVal;
-                this.parsePassDown();
-                break;
-            case disabled:
-                this._disabled = newVal !== null;
-                break;
+    
+        _passDown: string;
+        get passDown() {
+            return this._passDown;
         }
-    }
-    de(name: string, detail: any) {
-        const newEvent = new CustomEvent(name + '-changed', {
-            detail: detail,
-            bubbles: true,
-            composed: false,
-        } as CustomEventInit);
-        this.dispatchEvent(newEvent);
-        return newEvent;
-    }
-    _cssPropMap: ICssPropMap[];
-    parsePassDown() {
-        this._cssPropMap = [];
-        const splitPassDown = this._passDown.split('};');
-        splitPassDown.forEach(passDownSelectorAndProp => {
-            if (!passDownSelectorAndProp) return;
-            const splitPassTo2 = passDownSelectorAndProp.split('{');
-            this._cssPropMap.push({
-                cssSelector: splitPassTo2[0],
-                propTarget: splitPassTo2[1]
-            });
-        })
-
-    }
-    passDownProp(val: any) {
-        let nextSibling = this.nextElementSibling;
-        while (nextSibling) {
-            this._cssPropMap.forEach(map => {
-                if (nextSibling.matches(map.cssSelector)) {
-                    nextSibling[map.propTarget] = val;
+        set passDown(val) {
+            this.setAttribute(pass_down, val);
+        }
+    
+        _disabled: boolean;
+        get disabled() {
+            return this._disabled;
+        }
+        set disabled(val) {
+            if (val) {
+                this.setAttribute(disabled, '');
+            } else {
+                this.removeAttribute(disabled);
+            }
+        }
+    
+        attributeChangedCallback(name: string, oldVal: string, newVal: string) {
+            switch (name) {
+                case pass_down:
+                    this._passDown = newVal;
+                    this.parsePassDown();
+                    break;
+                case disabled:
+                    this._disabled = newVal !== null;
+                    break;
+            }
+        }
+        de(name: string, detail: any) {
+            const newEvent = new CustomEvent(name + '-changed', {
+                detail: detail,
+                bubbles: true,
+                composed: false,
+            } as CustomEventInit);
+            this.dispatchEvent(newEvent);
+            return newEvent;
+        }
+        _cssPropMap: ICssPropMap[];
+        parsePassDown() {
+            this._cssPropMap = [];
+            const splitPassDown = this._passDown.split('};');
+            splitPassDown.forEach(passDownSelectorAndProp => {
+                if (!passDownSelectorAndProp) return;
+                const splitPassTo2 = passDownSelectorAndProp.split('{');
+                this._cssPropMap.push({
+                    cssSelector: splitPassTo2[0],
+                    propTarget: splitPassTo2[1]
+                });
+            })
+    
+        }
+        passDownProp(val: any) {
+            let nextSibling = this.nextElementSibling;
+            while (nextSibling) {
+                this._cssPropMap.forEach(map => {
+                    if (nextSibling.matches(map.cssSelector)) {
+                        nextSibling[map.propTarget] = val;
+                    }
+                })
+                nextSibling = nextSibling.nextElementSibling;
+            }
+        }
+        _upgradeProperties(props: string[]) {
+            props.forEach(prop => {
+                if (this.hasOwnProperty(prop)) {
+                    let value = this[prop];
+                    delete this[prop];
+                    this[prop] = value;
                 }
             })
-            nextSibling = nextSibling.nextElementSibling;
+    
         }
-    }
-    _upgradeProperties(props: string[]) {
-        props.forEach(prop => {
-            if (this.hasOwnProperty(prop)) {
-                let value = this[prop];
-                delete this[prop];
-                this[prop] = value;
-            }
-        })
-
     }
 }
