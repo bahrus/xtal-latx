19b3ed3ee6d20d2a2b282f749002b7449d66e6ef
bahrus <anderson.bruce.b@gmail.com>
2018-08-31 20:59:36 -0400

    ...

---
+++ getHost.js
@@ -0,0 +1,13 @@
+export function getHost(el) {
+    let parent = el;
+    while (parent = (parent.parentNode)) {
+        if (parent.nodeType === 11) {
+            return parent['host'];
+        }
+        else if (parent.tagName === 'BODY') {
+            return null;
+        }
+    }
+    return null;
+}
+//# sourceMappingURL=getHost.js.map
