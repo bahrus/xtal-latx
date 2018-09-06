e1fc5245495492ebf6374dbd732ff8efd232bb13
bahrus <anderson.bruce.b@gmail.com>
2018-09-06 12:25:05 -0400

    ...

--- xtal-latx.ts
+++ xtal-latx.ts
@@ -1,7 +1,10 @@
 const disabled = 'disabled';
 
 type Constructor<T = {}> = new (...args: any[]) => T;
-
+/**
+ * Base class for many xtal- components
+ * @param superClass
+ */
 export function XtallatX<TBase extends Constructor<HTMLElement>>(superClass: TBase) {
     return class extends superClass {
         static get observedAttributes() {
@@ -9,22 +12,40 @@ export function XtallatX<TBase extends Constructor<HTMLElement>>(superClass: TBa
         }    
     
         _disabled!: boolean;
+        /**
+         * Any component that emits events should not do so ef it is disabled.
+         * Note that this is not enforced, but the disabled property is made available.
+         * Users of this mix-in sure ensure it doesn't call "de" if this property is set to true.
+         */
         get disabled() {
             return this._disabled;
         }
         set disabled(val) {
             this.attr(disabled, val, '');
         }
-
+        /**
+         * Set attribute value.
+         * @param name 
+         * @param val 
+         * @param trueVal String to set attribute if true.
+         */
         attr(name: string, val: string | boolean, trueVal?: string){
             const v = val ? 'set' : 'remove';  //verb
             (<any>this)[v + 'Attribute'](name, trueVal || val);
         }
         _evCount: {[key: string] : number} = {};
+        /**
+         * Turn number into string with even and odd values easy to query via css.
+         * @param n 
+         */
         to$(n: number){
             const mod = n % 2;
             return (n - mod) / 2 + '-' + mod;
         }
+        /**
+         * Increment event count
+         * @param name
+         */
         incAttr(name: string){
             const ec = this._evCount;
             if(name in ec) {
@@ -41,8 +62,14 @@ export function XtallatX<TBase extends Constructor<HTMLElement>>(superClass: TBa
                     break;
             }
         }
-        de(name: string, detail: any) {
-            const eventName = name + '-changed';
+        /**
+         * Dispatch Custom Event
+         * @param name Name of event to dispatch (with -changed if asIs is false)
+         * @param detail Information to be passed with the event
+         * @param asIs If true, don't append event name with '-changed'
+         */
+        de(name: string, detail: any, asIs?: boolean) {
+            const eventName = name + (asIs ? '' : '-changed');
             const newEvent = new CustomEvent(eventName, {
                 detail: detail,
                 bubbles: true,
@@ -53,6 +80,10 @@ export function XtallatX<TBase extends Constructor<HTMLElement>>(superClass: TBa
             return newEvent;
         }
 
+        /**
+         * Needed for asynchronous loading
+         * @param props Array of property names to "upgrade", without losing value set while element was Unknown
+         */
         _upgradeProperties(props: string[]) {
             props.forEach(prop => {
                 if (this.hasOwnProperty(prop)) {
