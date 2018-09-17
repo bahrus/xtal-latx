e20906375888c47beeec7bfe2f36e45e46f89fd2
bahrus <anderson.bruce.b@gmail.com>
2018-09-17 09:29:37 -0400

    ...

--- destruct.IE11.js
+++ destruct.IE11.js
@@ -1,14 +1,14 @@
 self['xtal_latx_ied'] = function(s){
     var split1 = s.split(';');
     var lhs = split1[0];
-    var re = /ref.[a-z_A-Z]*/g;
+    var re = /ref\.[a-z_A-Z]*/g;
     var args = [];
     while ((array1 = re.exec(lhs)) !== null) {
         args.push(array1[0].replace('ref.', ''))
     }
     const iOBPos = s.indexOf('{');
     const iLastCBPos = s.lastIndexOf('}');
-    var body = '{' + s.substring(iOBPos) + '}';
+    var body = s.substring(iOBPos, iLastCBPos + 1);
     return {
         args: args,
         body: body
