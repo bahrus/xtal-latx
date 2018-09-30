d163a88449ece98e5f466bdcdc84a0e6739010bf
bahrus <anderson.bruce.b@gmail.com>
2018-09-30 04:49:54 -0400

    add wrap

---
+++ with-path.ts
@@ -0,0 +1,48 @@
+import {XtallatX} from './xtal-latx.js';
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+const with_path = 'with-path';
+
+/**
+ * Custom Element mixin that allows a property to be namespaced
+ * @param superClass 
+ */
+export function WithPath<TBase extends Constructor<HTMLElement>>(superClass: TBase) {
+    return class extends superClass {
+        _withPath!: string;
+        /**
+        * @type {string}
+        * object inside a new empty object, with key equal to this value.
+        * E.g. if the incoming object is {foo: 'hello', bar: 'world'}
+        * and with-path = 'myPath'
+        * then the source object which be merged into is:
+        * {myPath: {foo: 'hello', bar: 'world'}}
+        */
+        get withPath() {
+            return this._withPath;
+        }
+        set withPath(val) {
+            this.setAttribute(with_path, val);
+        }
+
+        wrap(obj: any){
+            if (this._withPath) {
+                let mergedObj = {} as any;
+                const retObj = mergedObj;
+                const splitPath = this._withPath.split('.');
+                const lenMinus1 = splitPath.length - 1;
+                splitPath.forEach((pathToken, idx) => {
+                    if(idx === lenMinus1){
+                        mergedObj[pathToken] = obj;
+                    }else{
+                        mergedObj = mergedObj[pathToken] = {};
+                    }
+                })
+                return retObj;
+            }else{
+                return obj;
+            }
+        }
+    }
+
+}
