f5b9331b9448adcbffd0486645234e9ce4fc48ee
bahrus <anderson.bruce.b@gmail.com>
2018-11-05 19:51:16 -0500

    recursive filterDown

--- filterDown.js
+++ filterDown.js
@@ -1,15 +1,30 @@
-export function filterDown(el, match, max) {
-    let c = 0;
-    let ns = el.nextElementSibling;
-    const ret = [];
+export function filterDown(el, match, max, recAtr = null, recMatch = null, topEl = null, acc = null, cnt = null) {
+    let c = cnt === null ? 0 : cnt;
+    let ns = el;
+    const ret = acc === null ? [] : acc;
     const isF = typeof match === 'function';
-    while (ns) {
+    while (ns !== null) {
         let isG = isF ? match(ns) : ns.matches(match);
         if (isG) {
             ret.push(ns);
             c++;
             if (c >= max)
                 return ret;
+        }
+        if (recAtr !== null) {
+            const attr = ns.getAttribute(recAtr);
+            if (attr !== null) {
+                let doIt = true;
+                if (recMatch !== null) {
+                    doIt = !recMatch(topEl);
+                }
+                if (doIt) {
+                    const fc = ns.firstElementChild;
+                    if (fc !== null) {
+                        filterDown(fc, match, max, recAtr, recMatch, topEl, ret, c);
+                    }
+                }
+            }
         }
         ns = ns.nextElementSibling;
     }
--- filterDown.ts
+++ filterDown.ts
@@ -1,15 +1,30 @@
-type test = (el: Element) => boolean;
-export function filterDown(el: HTMLElement, match: string | test, max: number){
-    let c = 0;
-    let ns = el.nextElementSibling;
-    const ret : Element[] = [];
+type test = (el: Element | null) => boolean;
+export function filterDown(el: Element | null, match: string | test, max: number, recAtr: string | null = null, recMatch: test | null = null, topEl: Element | null = null, acc: Element[] | null = null, cnt: number | null = null){
+    let c = cnt === null ?  0 : cnt;
+    let ns = el;
+    const ret : Element[] = acc === null ? [] : acc;
     const isF = typeof match === 'function';
-    while(ns){
+    while(ns !== null){
         let isG = isF ? (<any>match)(ns) : ns.matches(match as string);
         if(isG){
             ret.push(ns);
             c++;
             if(c >= max ) return ret;
+        }
+        if(recAtr !== null){
+            const attr = ns.getAttribute(recAtr);
+            if(attr !== null){
+                let doIt = true;
+                if(recMatch !== null){
+                    doIt = !recMatch(topEl)
+                }
+                if(doIt){
+                    const fc = ns.firstElementChild;
+                    if(fc !== null){
+                        filterDown(fc, match, max, recAtr, recMatch, topEl, ret, c)
+                    }
+                }
+            }
         }
         ns = ns.nextElementSibling;
     }
